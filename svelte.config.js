4bc3c4b129fd1c6e673a4efa15260f57156a5cfc
jvoberMCS <114746137+jvoberMCS@users.noreply.github.com>
2026-01-30 09:52:50 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -4,43 +4,42 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import { mdsvex } from 'mdsvex'
 
 const config = {
-    compilerOptions: {
-        runes: true,
-    },
-    // preprocess: [vitePreprocess({ script: true }), mdsvex()],
-    onwarn: (warning, handler) => {
-        // suppress warnings on `vite dev` and `vite build`; but even without this, things still work
-        if (warning.code === 'a11y-click-events-have-key-events') return
-        if (warning.code === 'a11y-no-static-element-interactions') return
-        if (
-            warning.code ===
-            'a11y_no_noninteractive_element_to_interactive_role'
-        )
-            return
-        if (warning.code === 'a11y_click_events_have_key_events') return
-        handler(warning)
-    },
-    kit: {
-        adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: '404.html', // Required for SPA-like routing on GH Pages
-            precompress: false,
-            strict: true
-        }),
-        alias: {
-            '@/*': './src/lib/',
-        },
-        paths: {
-            // Use your repo name (e.g., '/my-repo') if not using a custom domain
-            base: process.env.NODE_ENV === 'production' ? '/science' : '',
-        }
-    },
-    extensions: ['.svelte', '.svx'],
-    output: {
-        inlineDynamicImports: false,
-        bundleStrategy: 'inline'
-    }
+	compilerOptions: {
+		runes: true,
+	},
+	// preprocess: [vitePreprocess({ script: true }), mdsvex()],
+	onwarn: (warning, handler) => {
+		// suppress warnings on `vite dev` and `vite build`; but even without this, things still work
+		if (warning.code === 'a11y-click-events-have-key-events') return
+		if (warning.code === 'a11y-no-static-element-interactions') return
+		if (
+			warning.code ===
+			'a11y_no_noninteractive_element_to_interactive_role'
+		)
+			return
+		if (warning.code === 'a11y_click_events_have_key_events') return
+		handler(warning)
+	},
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			precompress: false,
+			strict: true
+		}),
+		alias: {
+			'@/*': './src/lib/',
+		},
+		paths: {
+			// Use your repo name (e.g., '/my-repo') if not using a custom domain
+			base: process.env.NODE_ENV === 'production' ? '/science' : '',
+		}
+	},
+	extensions: ['.svelte', '.svx'],
+	output: {
+		inlineDynamicImports: false,
+		bundleStrategy: 'inline'
+	}
 }
 
 export default config
